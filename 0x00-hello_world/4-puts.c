256ddab760f9f343d5928f9074b59be8597973d7
nathan <olnathan26@gmail.com>
2021-06-17 05:33:15 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+/**
+ * main-entry point
+ * 
+ * Return: Always o (Success)
+ */
+int main(void)
+{
+	puts("/"programming is like building a multilingual puzzle");
+	return (0);
+}
