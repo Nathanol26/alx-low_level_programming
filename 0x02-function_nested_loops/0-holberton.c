e21cd73868b3bc34612d7dbd225b28c8d65d81a5
nathan <olnathan26@gmail.com>
2021-06-22 04:15:16 -0700

    copy holberten

---
+++ 0x02-function_nested_loops/0-holberton.c
@@ -0,0 +1,19 @@
+#include"holberten.h"
+
+/**
+ * main-printthe world"Holberten"
+ *
+ * Return:Always 0(success)
+ */
+
+int main(void)
+{
+int i;
+char p[]+"Holberten"
+	 for(i=0;i< 9; i++)
+	 {
+		 _putchar(p[i]);
+	 }
+_putchar('\n')
+	return (0);
+}
